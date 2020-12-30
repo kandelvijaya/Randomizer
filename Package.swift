69bd0c2d3f4a4e3dfcfa2f21a7e9a1e561b15e93
kandelvijaya <vijaya.prakash.kandel@zalando.de>
2020-12-30 23:23:36 +0100

    made a dynamic library

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "Randomizer",
+            type: .dynamic,
             targets: ["Randomizer"]),
     ],
     dependencies: [ ],
